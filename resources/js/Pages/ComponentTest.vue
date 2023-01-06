5e24ec9f58f3d2d5961778d47281bdc9fc1a688a
moroo <moroo0308@gmail.com>
2023-01-06 11:00:45 +0900

    Inertiaテスト 機能追加

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,18 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
+
+const emitTest = e => console.log(e);
+
+</script>
+
+<template>
+  <div>
+    <GuestLayout>
+      <InputLabel value="件名"></InputLabel>
+      <TextInput modelValue="初期値が入ります" @update:modelValue="emitTest">
+      </TextInput>
+    </GuestLayout>
+  </div>
+</template>
---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Inertia } from '@inertiajs/inertia'
+
+defineProps({
+  id: String,
+  blog: Object,
+})
+
+const deleteConfirm = id => {
+  Inertia.delete(`/inertia/${id}`, {
+    onBefore: () => confirm('本当に削除しますか？')
+  })
+}
+
+
+</script>
+<template>
+  <div>
+    {{ id }}<br>
+    {{ blog.id }}<br>
+    {{ blog.title }}<br>
+    <button @click="deleteConfirm(blog.id)">削除</button>
+  </div>
+</template>
